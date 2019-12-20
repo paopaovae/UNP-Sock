7b738d50be28393f6b82fcba3233af05c0f73d53
paopaovae <vae99cht66@163.com>
2019-12-20 17:09:26 +0800

    add host_test

---
+++ host_test/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unp.h>
+
+int main()
+{
+    union {
+        short s;
+        char c[sizeof(short)];
+    }un;
+
+    un.s = 0x0102;
+    printf("%s:\n", CPU_VENDOR_OS);
+
+    if (sizeof(short) == 2){
+        if (un.c[0] == 1 && un.c[1] == 2)
+            printf("big-endian\n");
+        else if (un.c[0] == 2 && un.c[1] == 1)
+            printf("little-endian\n");
+        else
+            printf("unknown\n");
+    }
+    else{
+        printf("sizeof(short) = %d\n", sizeof(short));
+    }
+    printf("Hello world!\n");
+    return 0;
+}
